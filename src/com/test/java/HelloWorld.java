59fe51a4fe20f3223a745b5ca356d15cd7622336
a910773203 <as778899000@qq.com>
2020-08-28 00:56:47 +0800

    Initial commit

---
+++ src/com/test/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+        System.out.println();
+        System.out.println("args = " + args);
+    }
+}
+/*
+
+ */
